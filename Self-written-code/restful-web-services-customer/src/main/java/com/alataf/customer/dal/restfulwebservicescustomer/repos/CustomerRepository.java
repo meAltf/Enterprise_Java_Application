a2e5edbceb687d0424eae5f6f79f20396a961855
meAltf <alataf.dce.cs1@gmail.com>
2024-01-05 15:44:19 +0530

    create repository interface for CRUD operation

--- Self-written-code/restful-web-services-customer/src/main/java/com/alataf/customer/dal/restfulwebservicescustomer/repos/CustomerRepository.java
+++ Self-written-code/restful-web-services-customer/src/main/java/com/alataf/customer/dal/restfulwebservicescustomer/repos/CustomerRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.alataf.customer.dal.restfulwebservicescustomer.entities.Customer;
 
+//created repository interface and extends it with CrudRepository for CRUD operation of API.
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
 }
