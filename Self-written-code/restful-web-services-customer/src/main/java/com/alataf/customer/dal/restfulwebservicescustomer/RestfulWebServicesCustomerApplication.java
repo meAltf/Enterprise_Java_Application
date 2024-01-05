dd7fe20d6c6fe5966020badaba88e0d163c25157
meAltf <alataf.dce.cs1@gmail.com>
2024-01-05 15:35:37 +0530

    Initialize spring boot project for customer

--- Self-written-code/restful-web-services-customer/src/main/java/com/alataf/customer/dal/restfulwebservicescustomer/RestfulWebServicesCustomerApplication.java
+++ Self-written-code/restful-web-services-customer/src/main/java/com/alataf/customer/dal/restfulwebservicescustomer/RestfulWebServicesCustomerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestfulWebServicesCustomerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestfulWebServicesCustomerApplication.class, args);
+		System.out.println("This is an assignment to create a data access layer for customer");
 	}
 
 }
