95523fe19e9bfcb7aa9d9ca32656fb340aa3a92b
meAltf <alataf.dce.cs1@gmail.com>
2024-01-15 12:49:49 +0530

    created CrudRepository interface

---
+++ Self-written-code/locationweb/src/main/java/com/alataf/location/repos/LocationRepository.java
@@ -0,0 +1,16 @@
+package com.alataf.location.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.alataf.location.entities.Location;
+/*
+ * replace T with the actual entity class for which you want to create the repository, 
+ * and ID with the type of your entity's primary key.
+ * CrudRepository<T, ID>
+ * T- model class/entity--> here Location
+ * ID- primaryKey in database--> here integer
+ */
+
+public interface LocationRepository extends CrudRepository<Location, Integer> {
+
+}
